077d1ec9f4eab5dbb4d34ab83d875715fb8b3244
Maykr1 <eclark72713@gmail.com>
2025-05-07 20:44:45 -0400

    Added Spring files

---
+++ src/test/java/com/project/PizzaOrdering3/PizzaOrdering3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.PizzaOrdering3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOrdering3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
