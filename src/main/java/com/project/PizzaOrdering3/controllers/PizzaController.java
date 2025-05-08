4042ef66261f2599fc256b09ded4dc96392a7cea
Maykr1 <eclark72713@gmail.com>
2025-05-07 22:33:12 -0400

    Added some more "search by" methods

--- src/main/java/com/project/PizzaOrdering3/controllers/PizzaController.java
+++ src/main/java/com/project/PizzaOrdering3/controllers/PizzaController.java
@@ -1,5 +1,7 @@
 package com.project.PizzaOrdering3.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/pizzas")
@@ -35,18 +39,38 @@ public class PizzaController {
     public Iterable<Pizza> getAllPizzas() {
         return this.pizzaRepository.findAll();
     }
+    //curl http://localhost:8080/pizzas
 
     @GetMapping("/{id}")
     public Optional<Pizza> getPizzaById(@PathVariable("id") Integer id) {
         return this.pizzaRepository.findById(id);
     }
-    
+    //curl http://localhost:8080/pizzas/5
+
+    @GetMapping("/search")
+    public List<Pizza> searchPizzas(
+        @RequestParam(name="name", required=false) String name,
+        @RequestParam(name="size", required=false) String size,
+        @RequestParam(name="price", required=false) Double price
+    ) {
+        if (name != null) {
+            return this.pizzaRepository.findByName(name);
+        } else if (size != null) {
+            return this.pizzaRepository.findBySize(size);
+        } else if (price != null) {
+            return this.pizzaRepository.findByPrice(price);
+        } else {
+            return new ArrayList<>();
+        }
+    }
+    //Can search for something like curl http://localhost:8080/pizzas/search?name=Pepperoni%20Pizza
 
     @PostMapping("")
     public Pizza createPizza(@RequestBody Pizza pizza) {
         Pizza newPizza = this.pizzaRepository.save(pizza);
         return newPizza;
     }
+    //curl -X POST http://localhost:8080/pizzas -H "Content-Type: application/json" -d '{"name": "Three Cheese Pizza", "size": "medium", "price": 10.50}'
 
     @PutMapping("/{id}")
     public Pizza updatePizza(@PathVariable("id") Integer id, @RequestBody Pizza pizza) {
@@ -72,6 +96,8 @@ public class PizzaController {
         return updatedPizza;
     }
 
+    //curl -X PUT http://localhost:8080/pizzas/4 -H "Content-Type: application/json" -d '{"name": "Three Cheese Pizza", "size": "medium", "price": 10.50}'
+
     @DeleteMapping("/{id}")
     public Pizza deletePizza(@PathVariable("id") Integer id) {
         Optional<Pizza> pizzaToDeleteOptional = this.pizzaRepository.findById(id);
@@ -84,4 +110,5 @@ public class PizzaController {
         this.pizzaRepository.delete(pizzaToDelete);
         return pizzaToDelete;
     }
+    //curl -X DELETE http://localhost:8080/pizzas/5
 }
--- src/main/java/com/project/PizzaOrdering3/repositories/PizzaRepository.java
+++ src/main/java/com/project/PizzaOrdering3/repositories/PizzaRepository.java
@@ -1,8 +1,14 @@
 package com.project.PizzaOrdering3.repositories;
 
 import com.project.PizzaOrdering3.entities.Pizza;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface PizzaRepository extends CrudRepository<Pizza, Integer>  {
-    
+
+    List<Pizza> findByName(String name);
+    List<Pizza> findBySize(String size);
+    List<Pizza> findByPrice(Double price);
 }
